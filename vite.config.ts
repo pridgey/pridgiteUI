8175436c4829bdfd4b7ce88baa0a61baa4e25016
Taylor Pridgen <taylor@pridgey.dev>
2025-07-14 15:30:06 -0600

    main/ configure package.json

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
       fileName: "index",
     },
     rollupOptions: {
-      external: ["solid-js", "@kobalte/core"],
+      external: ["solid-js", "@kobalte/core", "solid-icons"],
     },
   },
 });
